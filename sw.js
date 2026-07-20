2560a4131e0de7c0dc7b5a81f1b22e92ef1543a0
elisapiseddu <elisa.piseddu@rwi-essen.de>
2026-07-20 13:33:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,3 @@
-'use strict';
-const CACHE = 'disability-iat-v3.1.0';
-const LOCAL_FILES = [
-  './', './index.html', './style.css', './app.js', './admin.html',
-  './manifest.json', './README.md', './DEPLOYMENT.md', './SCORING_AND_DATA.md',
-];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(LOCAL_FILES)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
-      if (response && response.ok) {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      }
-      return response;
-    }))
-  );
-});
+// Version 3.2 does not use a service worker.
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.registration.unregister()));
